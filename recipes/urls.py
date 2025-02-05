5dd9e2233ca1c8b0cf33b1a1d3dc905e5787ac2f
Nestaraus <nestaraus@bk.ru>
2025-02-05 22:01:33 +0300

    Initial commit3

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('add/', views.add_recipe, name='add_recipe'),
     path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
+    path('register/', views.register, name='register'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,9 @@ from .models import Recipe
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from .forms import RecipeForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
     recipes = Recipe.objects.order_by('?')[:5]
@@ -34,3 +37,15 @@ def edit_recipe(request, recipe_id):
     else:
         form = RecipeForm(instance=recipe)
     return render(request, 'recipes/edit_recipe.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт создан для {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'recipes/register.html', {'form': form})
