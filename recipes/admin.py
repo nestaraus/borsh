90752e2003b99fec1b040da37d3b152dc5f7358a
Nestaraus <nestaraus@bk.ru>
2025-02-06 00:12:37 +0300

    Initial commit6

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe, Category
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'cooking_time')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
