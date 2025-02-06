554c21b4772dd8e7886473a54eb17dd045d105a6
Nestaraus <nestaraus@bk.ru>
2025-02-06 20:07:39 +0300

    Initial commit7

--- recipe_site/urls.py
+++ recipe_site/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('recipes.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
